18dbfc2537e5c3480f73defddfc65b27662838c2
rbnicodev <rubenmsanchis.dev@gmail.com>
2022-12-29 15:10:58 +0100

    clean imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
+//3os
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card'
 import {MatButtonModule} from '@angular/material/button';
+import { PaginatorComponent } from './components/paginator/paginator.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 //ngrx
@@ -11,24 +18,19 @@ import { StoreModule } from '@ngrx/store';
 import { resultsReducer, pageReducer } from './state/reducers/results.reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DetailComponent } from './pages/detail/detail.component';
-import { ListComponent } from './pages/list/list.component';
-import { CardComponent } from './components/card/card.component';
-import { BasicInfoComponent } from './components/basic-info/basic-info.component';
-import { InfoComponent } from './components/info/info.component';
-import { NavComponent } from './shared/nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SearchComponent } from './components/search/search.component';
-import { HeadImageComponent } from './components/head-image/head-image.component';
-import { PokemonService } from './service/pokemon.service';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+//propios
 import { BottomNavComponent } from './shared/bottom-nav/bottom-nav.component';
-import { PaginatorComponent } from './components/paginator/paginator.component';
+import { PokemonService } from './service/pokemon.service';
+import { HeadImageComponent } from './components/head-image/head-image.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { InfoComponent } from './components/info/info.component';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './pages/list/list.component';
+import { BasicInfoComponent } from './components/basic-info/basic-info.component';
+import { DetailComponent } from './pages/detail/detail.component';
+
+
 
 @NgModule({
   declarations: [
